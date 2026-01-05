4092fd794c166dc46c5f59865e243c4eb00767cf
azeriand <andrearomeracosta4@gmail.com>
2026-01-05 23:57:33 +0100

    fix: fix button style

--- frontend/src/app/components/sidebar.tsx
+++ frontend/src/app/components/sidebar.tsx
@@ -11,7 +11,9 @@ export default function Sidebar() {
     <div className='w-full flex flex-col gap-y-4'>
         <Card noPadding appearance='ghost'>
             <img src='/azeriand.png' alt="Azeriand Logo" className='w-full'/>
-            <Card appearance='mate' color='red' intensity={500} style={{ color: 'white' }}>Hello! I'm Andrea lore ipsum bla bla bla</Card>
+            <Card appearance='mate' color='red' intensity={500} style={{ color: 'white' }}>
+                Hello! I'm Andrea lore ipsum bla bla bla
+            </Card>
         </Card>
         <Card appearance='mate' color='blue' intensity={700} style={{ color: 'black' }} className='text-lg text-center flex items-center gap-x-2 justify-center'>
             <MdWifiTethering/>
@@ -21,8 +23,8 @@ export default function Sidebar() {
             <Button appearance='mate' color='blue' intensity={700} icon={<FaFileDownload/>} label='Resume' style={{ color: 'black' }} className='col-span-6 w-full font-bold'/>
             <Button appearance='mate' color='blue' intensity={700} icon={<FaEnvelope/>} label="Let's talk!" style={{ color: 'black' }} className='col-span-6 w-full font-bold'/>
         </section>
-        <section className='grid grid-cols-4 grid-rows-2 justify-items-center w-full gap-4'>
-            <Button appearance='mate' color='blue' intensity={700} icon={<FaReact/>} style={{ color: 'black' }} className='w-full h-full'/>
+        <section className='grid grid-cols-4 grid-rows-2 justify-items-center w-full gap-1'>
+            <Button appearance='mate' color='blue' intensity={700} icon={<FaReact/>} style={{ color: 'black' }} className='w-full aspect-square'/>
             <Button appearance='mate' color='blue' intensity={700} icon={<FaReact/>} style={{ color: 'black' }} className='w-full h-full'/>
             <Button appearance='mate' color='blue' intensity={700} icon={<FaReact/>} style={{ color: 'black' }} className='w-full h-full'/>
             <Button appearance='mate' color='blue' intensity={700} icon={<FaReact/>} style={{ color: 'black' }} className='w-full h-full'/>
