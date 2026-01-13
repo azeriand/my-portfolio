20b0374069eceb59cad743249edcf03b504fec9d
azeriand <andrearomeracosta4@gmail.com>
2026-01-13 18:11:48 +0100

    feat: redirect resume button to my resume

--- frontend/src/app/components/topbar.tsx
+++ frontend/src/app/components/topbar.tsx
@@ -8,7 +8,8 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Topbar() {
 
-    const myResumeLink = 'https://www.youtube.com/watch?v=oHg5SJYRHA0&t=2s'
+    const myResumeLink = () => {window.open('https://www.youtube.com/watch?v=oHg5SJYRHA0&t=2s')}
+    
     return (
         <Card appearance='mate' noPadding color='blue' intensity={700} className='flex justify-between items-center py-2 px-5 rounded-xl'>
             <div className='flex gap-x-8'>
@@ -17,7 +18,7 @@ export default function Topbar() {
                     <Link href='/projects'><Button color='blue' intensity={700} label="Projects" style={{ color: 'black' }}/></Link>
                     <Link href='/library'><Button color='blue' intensity={700} label="Library" style={{ color: 'black' }}/></Link>
                     <Link href='/articles'><Button color='blue' intensity={700} label="Articles" style={{ color: 'black' }}/></Link>
-                    <Button color='blue' intensity={700} label="Resume" style={{ color: 'black' }} onClick={window.open(myResumeLink)}/>
+                    <Button color='blue' intensity={700} label="Resume" style={{ color: 'black' }} onClick={myResumeLink}/>
                 </div>
             </div>
             <div className='flex gap-x-3 justify-end'>
