75d1bbf9c24f5f18765fa4833c346e3fecebdced
azeriand <andrearomeracosta4@gmail.com>
2026-02-04 16:15:40 +0100

    delete library page

--- frontend/src/app/library/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Library() {
-    return (
-        <div>
-            <h1>Library Page</h1>
-            <p>This is where my design library will be showcased.</p>
-        </div>
-    )
-}
