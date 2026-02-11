f5bfe7fab5e8d0528634d2349ba040f52a9967bc
azeriand <andrearomeracosta4@gmail.com>
2026-02-11 18:30:08 +0100

    style: style article data

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import 'azeriand-library/dist/styles.css';
 import "./globals.css";
+import "./prose.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
