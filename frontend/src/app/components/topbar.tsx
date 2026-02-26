9e9cb8f3de939544ab08d2bd408b764cd7ac1c90
azeriand <andrearomeracosta4@gmail.com>
2026-02-26 17:32:53 +0100

    style: add transitions

--- frontend/src/app/components/topbar.tsx
+++ frontend/src/app/components/topbar.tsx
@@ -18,7 +18,7 @@ export default function Topbar() {
     return (
         <Card appearance='mate' noPadding color='red' intensity={200} className='flex justify-between items-center py-2 px-5 rounded-xl'>
             <div className='flex gap-x-8'>
-                <Link href='/'><Avatar src="/azeriand.jpg"/></Link>
+                <Link href='/'><Avatar src="/azeriand.jpg" className='card'/></Link>
                 <div className='flex gap-x-3 items-center'>
                     <Link href='/projects'><Button appearance='ghost' color='red' intensity={600} label="Projects" style={{ color: 'black' }}/></Link>
                     <Link href='/articles'><Button appearance='ghost' color='red' intensity={600} label="Articles" style={{ color: 'black' }}/></Link>
