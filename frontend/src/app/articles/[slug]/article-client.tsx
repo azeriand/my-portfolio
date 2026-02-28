734c6706fca9834f392c3c3c074a47864b2c55a9
azeriand <andrearomeracosta4@gmail.com>
2026-02-28 19:02:16 +0100

    feat: add related articles in the article page

--- frontend/src/app/articles/[slug]/article-client.tsx
+++ frontend/src/app/articles/[slug]/article-client.tsx
@@ -1,16 +1,19 @@
 'use client';
 
-import { Card } from 'azeriand-library';
+import { Card, Badge } from 'azeriand-library';
+import Link from 'next/link';
 import ReactMarkdown from "react-markdown";
 
 interface Article {
     id: number;
     title: string;
-    content: string;
-    cover: string | undefined;
+    content?: string;
+    slug?: string;
+    cover?: string;
+    description?: string;
 }
 
-function RichText({ content }: { content: string }) {
+function RichText({ content }: { content?: string }) {
   if (!content) return null;
 
   return (
@@ -21,10 +24,11 @@ function RichText({ content }: { content: string }) {
 }
 
 interface ArticleClientProps {
-  article: Article | null;
+  article?: Article | null;
+  lastArticles?: Article[];
 }
 
-export function ArticleClient({ article }: ArticleClientProps) {
+export function ArticleClient({ article, lastArticles }: ArticleClientProps) {
   if (!article) {
     return (
       <Card appearance="mate" color="red" intensity={500} className='justify-center'>
@@ -33,15 +37,31 @@ export function ArticleClient({ article }: ArticleClientProps) {
       </Card>
     );
   }
-
+  console.log("articles array", lastArticles); // Log the article data to ensure it's being passed correctly
   return (
     <Card appearance="mate" color="orange" intensity={500} className='justify-center'>
         <img src={article.cover} alt='Article image' className='w-full h-80! object-cover rounded-lg'/>
-        <article className='flex flex-col gap-y-4 mt-8 px-4'>
+        <article className='flex flex-col md:flex-row! gap-4 mt-8 px-4'>
+          <div className='basis-2/3'>
             <h1 style={{ color: 'black' }} className='font-bold text-4xl!'>{article.title}</h1>
             <div style={{ color: 'black' }} className='w-full flex flex-col flex-wrap items-center'>
                 <RichText content={article.content}/>
             </div>
+          </div>
+          <div className='basis-1/3'>
+            <h2 style={{ color: 'black' }} className='font-bold text-2xl! mb-4'>Related Articles</h2>
+            {lastArticles?.map(article => 
+              <Link href={`/articles/${article.slug}`} key={article.id}>
+                <Card appearance="mate" color="yellow" intensity={500} noPadding key={article.id} className='flex flex-col gap-y-2 p-4 rounded-lg'>
+                  <Badge appearance='mate' size='sm' color='orange' intensity={800} label={'Design Library'}/>
+                  <article className='flex flex-col gap-y-1'>
+                    <h3 style={{ color: 'black'}} className='font-bold'>{article.title}</h3>
+                    <p style={{ color: 'black'}}>{article.description}</p>
+                  </article>
+                </Card>
+              </Link>
+            )}
+          </div>
         </article>
     </Card>
   )
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,17 @@
 import client from "../../strapi";
 import Homepage from "./components/homepage";
+import {ArticleClient} from '@/app/articles/[slug]/article-client';
 
-async function getData() {
+export async function getData() {
   const result = await client.collection('articles').find({populate: '*'});
   const articles = result.data;
   const maxArticlesShowed = 3
   const lastArticles = articles.slice(articles.length - maxArticlesShowed, articles.length).reverse();
-
-  return lastArticles as any[];
+  return lastArticles;
 }
 
 export default async function Home() {
   const articles = await getData();
 
-  return <Homepage articles={articles} />;
+  return <Homepage articles={articles as any} />;
 }
